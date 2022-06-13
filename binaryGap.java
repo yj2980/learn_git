901933982d11e553be1b5c70a7293c8f93b62b18
yj2980 <adore2100@naver.com>
2022-06-14 03:19:33 +0900

    BinaryGap

---
+++ binaryGap.java
@@ -0,0 +1,37 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int N) {
+        int devN = N;
+        int cnt, result;
+        boolean p = false; 
+
+        cnt = 0;
+        result = 0;
+
+        while (N >= 1)
+        {
+            devN = N % 2;
+            if (devN == 1)
+            {
+                if (cnt >= 1)
+                {
+                    if (result < cnt)
+                    {
+                        result = cnt;
+                        cnt = 0;
+                    }
+                }
+                p = true;
+            }
+            else if (p)
+                cnt++;
+            N /= 2;
+        }
+        return result;
+    }
+}
