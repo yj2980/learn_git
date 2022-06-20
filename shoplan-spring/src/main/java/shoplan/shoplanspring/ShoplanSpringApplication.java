1c46c9d3aacf24e24adb949b0171efb372e0bb2e
yj2980 <adore2100@naver.com>
2022-06-20 23:40:11 +0900

    Home.html UI configuration (Topbar , Navbar)

---
+++ shoplan-spring/src/main/java/shoplan/shoplanspring/ShoplanSpringApplication.java
@@ -0,0 +1,13 @@
+package shoplan.shoplanspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoplanSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoplanSpringApplication.class, args);
+	}
+
+}
---
+++ shoplan-spring/src/test/java/shoplan/shoplanspring/ShoplanSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package shoplan.shoplanspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoplanSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
