8b93d826571f5329b23f6be026ffe0cddc1cbd27
yj2980 <adore2100@naver.com>
2022-10-24 14:37:36 +0900

    remove file

--- board_01/src/main/java/board_example/board_01/domain/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package board_example.board_01.domain.repository;
-// 데이터의 조작을 담당하며 JPA 상속을 받음
-import board_example.board_01.domain.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
--- board_01/src/main/java/board_example/board_01/domain/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package board_example.board_01.domain.repository;
-
-import board_example.board_01.domain.entity.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long> {
-}
